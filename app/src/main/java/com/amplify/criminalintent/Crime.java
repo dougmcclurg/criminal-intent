939c6146681f44a79760a2949a7ac24df0ee38a3
Douglas McClurg <doug.mcclurg@gmail.com>
2014-07-09 17:15:19 -0400

    Long press menu

--- app/src/main/java/com/amplify/criminalintent/Crime.java
+++ app/src/main/java/com/amplify/criminalintent/Crime.java
@@ -1,11 +1,19 @@
 package com.amplify.criminalintent;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE = "date";
 
     private UUID id;
     private String title;
@@ -15,6 +23,24 @@ public class Crime {
     public Crime() {
         id = UUID.randomUUID();
         date = new Date();
+    }
+
+    public Crime(JSONObject json) throws JSONException {
+        id = UUID.fromString(json.getString(JSON_ID));
+        if (json.has(JSON_TITLE)) {
+            title = json.getString(JSON_TITLE);
+        }
+        solved = json.getBoolean(JSON_SOLVED);
+        date = new Date(json.getLong(JSON_DATE));
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, id.toString());
+        json.put(JSON_TITLE, title);
+        json.put(JSON_SOLVED, solved);
+        json.put(JSON_DATE, date.getTime());
+        return json;
     }
 
     @Override
--- app/src/main/java/com/amplify/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/amplify/criminalintent/CrimeFragment.java
@@ -1,14 +1,18 @@
 package com.amplify.criminalintent;
 
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -42,19 +46,47 @@ public class CrimeFragment extends Fragment {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(getActivity()) != null) {
+                    NavUtils.navigateUpFromSameTask(getActivity());
+                }
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        CrimeLab.get(getActivity()).saveCrimes();
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
         crime = CrimeLab.get(getActivity()).getCrime(crimeId);
+        setHasOptionsMenu(true);
     }
 
     private void updateDate() {
         dateButton.setText(crime.getDate().toString());
     }
 
+    @TargetApi(11)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            if (NavUtils.getParentActivityName(getActivity()) != null) {
+                getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        }
 
         titleField = (EditText)v.findViewById(R.id.crime_title);
         titleField.setText(crime.getTitle());
